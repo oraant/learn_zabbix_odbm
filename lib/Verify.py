0808d3ced6801e04fb752ae82f8e1c33fdc33f01
oraant <oraant777@gmail.com>
2016-03-18 11:40:22 +0800

    modify sequence of some method.

--- lib/Verify.py
+++ lib/Verify.py
@@ -29,18 +29,6 @@ class Verify:
         output.close()
         return passwd
     
-    def verify_passwd(self):
-        '''判断密码是否正确'''
-        print '\n  The encrypted text is:'
-        print '  \033[1;31;31m' + self.__generate_code() + '\033[0m'
-        password = raw_input('  Please input password for zodbm:')
-        if password == self.__generate_passwd().strip('\n'):
-            print '  Verify \033[4mpassed\033[0m.\n'
-            return True
-        else:
-            print '  Verify \033[4mfailed\033[0m.\n'
-            return False
-    
     def __passwd_code(self,code):
         '''根据加密后的字符串算出密码'''
         code = base64.decodestring(code)
@@ -53,6 +41,18 @@ class Verify:
         output.close()
         return passwd
 
+    def verify_passwd(self):
+        '''判断密码是否正确'''
+        print '\n  The encrypted text is:'
+        print '  \033[1;31;31m' + self.__generate_code() + '\033[0m'
+        password = raw_input('  Please input password for zodbm:')
+        if password == self.__generate_passwd().strip('\n'):
+            print '  Verify \033[4mpassed\033[0m.\n'
+            return True
+        else:
+            print '  Verify \033[4mfailed\033[0m.\n'
+            return False
+    
 if __name__ == '__main__':
     v = Verify()
     v.verify_passwd()
