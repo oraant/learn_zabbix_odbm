9861483612c7733c9c1aa1042e00bfb33d2af604
oraant <oraant777@gmail.com>
2016-03-18 09:29:44 +0800

    migrate verify part into verify.py,add unit testing model.

--- lib/verify.py
+++ lib/verify.py
@@ -13,7 +13,7 @@ class Verify:
         output.close()
         return text
     
-    def generate_code(self):
+    def __generate_code(self):
         '''将日期-MAC字符串加密'''
         text = self.__generate_text()
         text = base64.encodestring(text)
@@ -29,11 +29,16 @@ class Verify:
         output.close()
         return passwd
     
-    def verify_passwd(self,password):
+    def verify_passwd(self):
         '''判断密码是否正确'''
+        print '\n  The encrypted text is:'
+        print '  \033[1;31;31m' + self.__generate_code() + '\033[0m'
+        password = raw_input('  Please input password for zodbm:')
         if password == self.__generate_passwd().strip('\n'):
+            print '  Verify \033[4mpassed\033[0m.\n'
             return True
         else:
+            print '  Verify \033[4mfailed\033[0m.\n'
             return False
     
     def __passwd_code(self,code):
@@ -47,3 +52,7 @@ class Verify:
         passwd = output.read()
         output.close()
         return passwd
+
+if __name__ == '__main__':
+    v = Verify()
+    v.verify_passwd()
