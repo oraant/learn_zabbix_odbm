7ee7bcc81e36c6a402f2efb7666f83a017a85413
oraant <oraant777@gmail.com>
2016-03-21 17:23:53 +0800

    complete oracle connections manager.

--- lib/ConnsManager.py
+++ lib/ConnsManager.py
@@ -2,79 +2,89 @@
 
 class ConnsManager:
 
-    def __init__():
+    def __init__(self):
         self.conns = {}
 
-    def __create_conn(self,link):
-        raise NotImplementedError,"Class %s does not implement required function" % self.__class__
+    def _create_conn(self,link):
+        raise NotImplementedError,"Class %s does not implement required function" % self._class_
         conn=''
         return conn
 
-    def __test_conn(self,conn):
-        raise NotImplementedError,"Class %s does not implement required function" % self.__class__
+    def _test_conn(self,conn):
+        raise NotImplementedError,"Class %s does not implement required function" % self._class_
         useable=''
         return useable
 
-    def __use_conn(self,conn,sql):
-        raise NotImplementedError,"Class %s does not implement required function" % self.__class__
+    def _use_conn(self,conn,sql):
+        raise NotImplementedError,"Class %s does not implement required function" % self._class_
         datas = ''
         return datas
 
-    def __close_conn(self,conn):
-        raise NotImplementedError,"Class %s does not implement required function" % self.__class__
+    def _close_conn(self,conn):
+        raise NotImplementedError,"Class %s does not implement required function" % self._class_
 
     def get_conn(self,name,link):
-        if name in self.dict.keys():
-            return self.dict[name]
+        if name in self.conns.keys():
+            print self.conns ############
+            return self.conns[name]
         try:
-            conn = self.__create_conn(link)
-            self.dict[name] = conn
+            conn = self._create_conn(link)
+            self.conns[name] = conn
+            print self.conns ############
             return conn
-        except:
+        except Exception,e:
+            print e ############
             return False
 
     def reget_conn(self,name,link):
-        self.close_conn(name)
+        self.clear_conn(name)
         self.get_conn(name,link)
 
-    def clear_conn(self,name)
-        conn = self.dict.pop(name)
-        self.__close_conn(conn)
+    def clear_conn(self,name):
+        conn = self.conns.pop(name)
+        self._close_conn(conn)
 
-    def clear_conns(self)
-        for name in self.dict.keys():
+    def clear_conns(self):
+        for name in self.conns.keys():
             self.clear_conn(name)
 
 class OracleConns(ConnsManager):
-    #dict = {}
-    dict = {
-        'link1':'conn1',
-        'link2':'conn2',
-        'link3':'conn3',
-        'link4':'conn4',
-        'link5':'conn5',
-        }
 
-    def __create_conn(self,link)
+    def _create_conn(self,link):
         import cx_Oracle
-        conn = cx_Oracle.connect(link)
+        conn = cx_Oracle.Connection(link)
         return conn
 
-    def get_or_create_conn(self,name,target):
-        if name in self.dict.keys():
-            return self.dict[name]
+    def _test_conn(self,conn):
+        sql='select open_mode from v$database'
+        try:
+            datas = self._use_conn(conn,sql)
+        except:
+            return False
+
+        if datas == 'READ WRITE':
+            return True
         else:
-            conn = self.__create_conn(target)
-            self.dict[name] = conn
-            return conn
+            return False
 
-    def use_conn(self,name,sql):
-        return self.get_conn(name).execute(sql).fetchall()
+    def _use_conn(self,conn,sql):
+        cursor = conn.cursor()
+        cursor.execute(sql)
+        datas = cursor.fetchall()
+        cursor.close()
+        return datas
 
-    def close_conn(self,name)
-        self.dict[name].close()
+    def _close_conn(self,conn):
+        conn.close()
 
-    def close_conns(self)
-        for name in self.dict.keys():
-            self.close_conn(name)
-
+if __name__ == '__main__':
+    tnsname = 'system/oracle@192.168.18.129:1521/db11g'
+    ocns = OracleConns()
+    import time
+    for i in range(5):
+        print 'start'
+        time.sleep(3)
+        conn = ocns.get_conn(tnsname,tnsname)
+    print conn
+    ocns.clear_conns()
+    #print '##########'
